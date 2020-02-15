e3c348809cb54f4617267629958d2853e71bc25c
Kerim Satirli <ksatirli@users.noreply.github.com>
2020-02-15 17:57:43 +0100

    adds GitHub Actions (#8)
    * adds GitHub Actions
    
    * ignore rule `SC2068` for `shellcheck`

--- oh-release.sh
+++ oh-release.sh
@@ -5,6 +5,7 @@ SELF=$(realpath "${0}")
 # shellcheck source=./config/oh-defaults.sh
 . "$(dirname "${SELF}")/config/oh-defaults.sh"
 
+# shellcheck disable=SC2068
 release-it \
   --config="${RELEASEIT_CONFIG}" \
   ${@}
